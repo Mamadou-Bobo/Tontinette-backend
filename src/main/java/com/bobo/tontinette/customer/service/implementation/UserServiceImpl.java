76b77e12855812e1f3547542b5ea55cc49dc5626
Mamadou-Bobo <diallomamadoubobo999@gmail.com>
2023-11-03 12:04:29 +0800

    implementing response handler service

--- src/main/java/com/bobo/tontinette/customer/service/implementation/UserServiceImpl.java
+++ src/main/java/com/bobo/tontinette/customer/service/implementation/UserServiceImpl.java
@@ -5,6 +5,7 @@ import com.bobo.tontinette.customer.dto.mapper.UserDTOMapper;
 import com.bobo.tontinette.customer.entity.User;
 import com.bobo.tontinette.customer.repository.UserRepository;
 import com.bobo.tontinette.customer.service.UserService;
+import com.bobo.tontinette.shared.utils.ResponseHandlerService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -23,6 +24,7 @@ class UserServiceImpl implements UserService {
 
     private final UserDTOMapper userDTOMapper;
     private final UserRepository userRepository;
+    private final ResponseHandlerService responseHandlerService;
 
     @Override
     public ResponseEntity<String> addUser(UserDTO userDTO) {
@@ -35,29 +37,32 @@ class UserServiceImpl implements UserService {
         if(userRepository.findByPhoneNumber(user.getPhoneNumber()).isPresent()) {
             message = "Phone number already exists";
             log.error(message);
-            return new ResponseEntity<>(message, HttpStatus.CONFLICT);
+            return responseHandlerService.handleError(message,HttpStatus.CONFLICT);
         }
 
         if(userRepository.findByEmail(user.getEmail()).isPresent()) {
             message = "Email address already exists";
             log.error(message);
-            return new ResponseEntity<>(message, HttpStatus.CONFLICT);
+            return responseHandlerService.handleError(message,HttpStatus.CONFLICT);
         }
 
         if(user.getPassword().isBlank()) {
             message = "Please enter your password";
             log.error(message);
-            return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
+            return responseHandlerService.handleError(message,HttpStatus.BAD_REQUEST);
         }
 
         if(user.getRoles() == null) {
-            message = "User role cannot be empty";
+            message = "User should be assigned a role";
             log.error(message);
-            return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
+            return responseHandlerService.handleError(message,HttpStatus.BAD_REQUEST);
         }
 
         userRepository.save(user);
 
-        return ResponseEntity.ok("User account successfully created");
+        return responseHandlerService.handleError(
+                "User account successfully created",
+                HttpStatus.CREATED
+        );
     }
 }
