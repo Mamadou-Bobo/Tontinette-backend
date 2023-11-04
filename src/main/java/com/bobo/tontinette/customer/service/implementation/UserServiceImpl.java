1623e8afec1c9d49ecb267990db89ed9ddf34503
Mamadou-Bobo <diallomamadoubobo999@gmail.com>
2023-11-04 12:21:17 +0800

    refactoring register and updateUserAccount methods

--- src/main/java/com/bobo/tontinette/customer/service/implementation/UserServiceImpl.java
+++ src/main/java/com/bobo/tontinette/customer/service/implementation/UserServiceImpl.java
@@ -5,7 +5,6 @@ import com.bobo.tontinette.customer.dto.mapper.UserDTOMapper;
 import com.bobo.tontinette.customer.entity.User;
 import com.bobo.tontinette.customer.repository.UserRepository;
 import com.bobo.tontinette.customer.service.UserService;
-import com.bobo.tontinette.shared.utils.ResponseHandlerService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -24,7 +23,6 @@ class UserServiceImpl implements UserService {
 
     private final UserDTOMapper userDTOMapper;
     private final UserRepository userRepository;
-    private final ResponseHandlerService responseHandlerService;
 
     @Override
     public ResponseEntity<String> addUser(UserDTO userDTO) {
@@ -37,30 +35,30 @@ class UserServiceImpl implements UserService {
         if(userRepository.findByPhoneNumber(user.getPhoneNumber()).isPresent()) {
             message = "Phone number already exists";
             log.error(message);
-            return responseHandlerService.handleError(message,HttpStatus.CONFLICT);
+            return new ResponseEntity<>(message,HttpStatus.CONFLICT);
         }
 
         if(userRepository.findByEmail(user.getEmail()).isPresent()) {
             message = "Email address already exists";
             log.error(message);
-            return responseHandlerService.handleError(message,HttpStatus.CONFLICT);
+            return new ResponseEntity<>(message,HttpStatus.CONFLICT);
         }
 
         if(user.getPassword().isBlank()) {
             message = "Please enter your password";
             log.error(message);
-            return responseHandlerService.handleError(message,HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(message,HttpStatus.BAD_REQUEST);
         }
 
         if(user.getRoles() == null) {
             message = "User should be assigned a role";
             log.error(message);
-            return responseHandlerService.handleError(message,HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(message,HttpStatus.BAD_REQUEST);
         }
 
         userRepository.save(user);
 
-        return responseHandlerService.handleError(
+        return new ResponseEntity<>(
                 "User account successfully created",
                 HttpStatus.CREATED
         );
@@ -68,33 +66,31 @@ class UserServiceImpl implements UserService {
 
     @Override
     public ResponseEntity<String> updateUserAccount(UserDTO userDTO) {
+        String message;
+
         if(userRepository.findByPhoneNumber(userDTO.phoneNumber()).isEmpty()) {
-            log.error("User does not exist");
-            return responseHandlerService.handleError("User does not exist", HttpStatus.NOT_FOUND);
+            message = "User does not exist";
+            log.error(message);
+            return new ResponseEntity<>(message,HttpStatus.NOT_FOUND);
+        }
+
+        // Make sure the given information match the one on the passport
+
+        if(userRepository.findByEmail(userDTO.email()).isPresent()) {
+            message = "Email address already exists";
+            log.error(message);
+            return new ResponseEntity<>(message,HttpStatus.NOT_FOUND);
         }
 
         User user = userRepository.findByPhoneNumber(userDTO.phoneNumber()).get();
 
-        // Make sure the given information match the one on the passport
-
-        // verify that the email is not taken
-
         user.setFirstName(userDTO.firstName());
         user.setLastName(userDTO.lastName());
         user.setPassword(userDTO.password());
-
-        if(userRepository.findByEmail(userDTO.email()).isPresent()) {
-            return responseHandlerService.handleError("Email address already exists", HttpStatus.CONFLICT);
-        }
-
         user.setEmail(userDTO.email());
 
         userRepository.save(user);
 
-        return responseHandlerService
-                .handleError(
-                        "User account successfully updated",
-                        HttpStatus.OK
-                );
+        return ResponseEntity.ok("User account successfully updated");
     }
 }
