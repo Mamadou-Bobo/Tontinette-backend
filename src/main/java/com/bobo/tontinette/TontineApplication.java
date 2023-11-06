131d5cfb44d12e3d6f4dd5da84778fdad8ecfffb
Mamadou-Bobo <diallomamadoubobo999@gmail.com>
2023-11-06 13:57:44 +0800

    refactoring command line runner method

--- src/main/java/com/bobo/tontinette/TontineApplication.java
+++ src/main/java/com/bobo/tontinette/TontineApplication.java
@@ -2,12 +2,11 @@ package com.bobo.tontinette;
 
 import com.bobo.tontinette.customer.entity.Privilege;
 import com.bobo.tontinette.customer.entity.Role;
-import com.bobo.tontinette.customer.repository.PrivilegeRepository;
-import com.bobo.tontinette.customer.repository.RoleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -16,32 +15,32 @@ import java.util.List;
 import static com.bobo.tontinette.shared.utils.Constant.*;
 
 @SpringBootApplication
-@RequiredArgsConstructor
-public class TontineApplication implements CommandLineRunner {
-
-	private final PrivilegeRepository privilegeRepository;
-	private final RoleRepository roleRepository;
+public class TontineApplication  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TontineApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) {
+	/*
+	@Bean
+	CommandLineRunner runner(RoleRepository roleRepository,
+							 PrivilegeRepository privilegeRepository) {
+		return args -> {
+			Collection<Privilege> privileges = new ArrayList<>();
 
-		Collection<Privilege> privileges = new ArrayList<>();
+			privileges.add(new Privilege(READ));
+			privileges.add(new Privilege(WRITE));
+			privileges.add(new Privilege(DELETE));
 
-		privileges.add(new Privilege(READ));
-		privileges.add(new Privilege(WRITE));
-		privileges.add(new Privilege(DELETE));
+			privilegeRepository.saveAll(privileges);
 
-		privilegeRepository.saveAll(privileges);
+			List<Role> roles = new ArrayList<>();
 
-		List<Role> roles = new ArrayList<>();
+			roles.add(new Role(ADMIN,privileges));
 
-		roles.add(new Role(ADMIN,privileges));
-
-		roleRepository.saveAll(roles);
+			roleRepository.saveAll(roles);
+		};
 	}
+	 */
 
 }
